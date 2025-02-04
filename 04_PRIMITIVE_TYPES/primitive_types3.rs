3a9e3eda5a92f30f0cc24590445af8cea5e053f3
Reham-Alsereidi <Rehamalsereidi@gmail.com>
2025-02-04 09:23:59 +0400

    Update primitive_types3.rs

--- 04_PRIMITIVE_TYPES/primitive_types3.rs
+++ 04_PRIMITIVE_TYPES/primitive_types3.rs
@@ -1,6 +1,6 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a = [1; 100];  //This creates an array with 100 elements, all set to 1
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
