9a603e6c47c0087c2ff458968c1f8d112f6e4368
Reham-Alsereidi <Rehamalsereidi@gmail.com>
2025-02-11 09:23:09 +0400

    Update if1.rs

--- 03_IF/if1.rs
+++ 03_IF/if1.rs
@@ -4,6 +4,16 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        println!("a is bigger");
+        return a;
+    } else if b > a {
+        println!("b is bigger");
+        return b;
+    } else {
+        println!("both numbers are equal");
+        return a;
+    }
 }
 
 fn main() {
