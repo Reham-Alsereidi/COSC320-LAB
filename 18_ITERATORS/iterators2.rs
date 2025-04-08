057704b3884f7ad828839308c3f927ce8d19c4f7
Reham-Alsereidi <Rehamalsereidi@gmail.com>
2025-04-08 09:27:52 +0400

    Update iterators2.rs

--- 18_ITERATORS/iterators2.rs
+++ 18_ITERATORS/iterators2.rs
@@ -3,11 +3,12 @@
 
 // TODO: Complete the `capitalize_first` function.
 // "hello" -> "Hello"
+use std::iter::Iterator;
 fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_uppercase().chain(chars).collect(),
     }
 }
 
@@ -16,6 +17,7 @@ fn capitalize_first(input: &str) -> String {
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
     // ???
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
@@ -23,6 +25,7 @@ fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
     // ???
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 fn main() {
