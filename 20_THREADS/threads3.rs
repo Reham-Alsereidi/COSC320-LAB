3aba54f2b5f41037fa5affb8adba9a1bd76d8ae6
Reham-Alsereidi <Rehamalsereidi@gmail.com>
2025-04-15 09:34:50 +0400

    Update threads3.rs

--- 20_THREADS/threads3.rs
+++ 20_THREADS/threads3.rs
@@ -1,4 +1,5 @@
 use std::{sync::mpsc, thread, time::Duration};
+use std::sync::Arc;
 
 struct Queue {
     first_half: Vec<u32>,
@@ -17,18 +18,24 @@ impl Queue {
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     // TODO: We want to send `tx` to both threads. But currently, it is moved
     // into the first thread. How could you solve this problem?
+    let qc = Arc::new(q);
+    let qc1 = qc.clone();
+    let qc2 = qc.clone();
+    let trans1 = tx.clone();
+    let trans2 = tx.clone();
+    
     thread::spawn(move || {
-        for val in q.first_half {
-            println!("Sending {val:?}");
-            tx.send(val).unwrap();
+        for val in &qc1.first_half {
+            println!("Sending {:?}", val);
+            trans1.clone().send(*val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
 
     thread::spawn(move || {
-        for val in q.second_half {
-            println!("Sending {val:?}");
-            tx.send(val).unwrap();
+        for val in &qc2.second_half {
+            println!("Sending {:?}",val);
+            trans2.send(*val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
